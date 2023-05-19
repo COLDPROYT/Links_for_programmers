88ff46d6ddb8feca19acbf6b437cb77568921bfe
COLDPROYT <moy.akkaunt22.08@gmail.com>
2023-05-19 18:57:21 +0300

    Added files

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Useful links for programmers!</h1>
+<h3>This repository contains very useful links for programmers, Java, Kotlin, TypeScript, and soon there will be more for other programming languages
---
+++ src/useful_links.java
@@ -0,0 +1,60 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Links for all programming languages
+
+/*
+
+1. https://coderbooks.ru/books/ - Programming books (Ada, Assembler, C, C#, C++, Cobol, CSS, Dart, Data Science, Delphi,
+Erlang, F#, Flutter, Fortran, Git, Go, Hacking and Security, Haskell, HTML, Java, JavaScript, Kotlin, Lua, Pascal, Perl,
+Php, Processing, Python, Q#, R, Ruby, Rust, Scala, SQL, Swift, UML, Xml) - There are all programming languages that are
+on this site.
+
+*/
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Useful links, books, videos on Java
+/*
+
+1. https://docs.oracle.com/javase/tutorial/index.html - Official documentation about the Java language
+2. https://disk.yandex.ru/i/IH16czbcc2KSkg - Book by Java (Downloaded from coder books)
+3. https://youtube.com/playlist?list=PLAma_mKffTOSUkXp26rgdnC0PicnmnDak - Java tutorial number 1
+4. https://youtube.com/playlist?list=PLZqgWWF4O-zgDLDBMva788aJf_vQ9GKJv - Java tutorial number 2 (This is a very best
+tutorial on Java (in my opinion))
+5. https://youtube.com/playlist?list=PL0lO_mIqDDFW2xXiWSfjT7hEdOUZHVNbK - Java tutorial number 3
+6. https://youtube.com/playlist?list=PLQwXjjTVqyUFAm2ZpCS20Oddpj85VIKFQ - Java tutorial number 4
+7. https://javarush.com/groups/posts - Many useful articles
+8. Follow white rabbit - The is a great YouTube channel on Java
+9. Herbert Schildt complete guide or beginner's guide - A good book on Java
+10. Kathy Sierra and Bert Bates "Learning Java 2nd Edition" - A good book on Java
+
+ */
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Useful links, books, videos on Kotlin
+
+/*
+
+1. https://kotlinlang.org/docs/books.html - Official documentation about the Kotlin language
+2. https://coderbooks.ru/books/kotlin/head_first_kotlin_griffits_2020/ - Book by Kotlin
+3. https://youtube.com/playlist?list=PLIU76b8Cjem4ZOt3tlWykUX1AjL9zE19t - Kotlin tutorial number 1
+4. https://youtube.com/playlist?list=PLyfVjOYzujuj20Y-3kVhT3Zro9CrMNgNS - Kotlin tutorial number 2
+
+
+
+
+ */
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Useful links, books, videos on TypeScript
+
+/*
+
+1. https://typescript-definitive-guide.ru/ - Official TypeScript documentation in Russian
+
+ */
+
+// To be continued... Comming soon...
+
+
+
+
+
